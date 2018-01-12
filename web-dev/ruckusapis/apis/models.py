417b2de0ef9e4fdfbcd97f2a0f5ee46bfd167044
pradam <pradamabhilash@gmail.com>
2018-01-12 18:54:38 +0530

    just committing the changes

--- web-dev/ruckusapis/apis/models.py
+++ web-dev/ruckusapis/apis/models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-"""
-Only admin users can add or delete products.
-Users can view all products.
-Users can buy a product.
-Users should be able to provide rating(1-5) to a product they've bought. They should not be able to add rating to any other product.
-Users should be able to change their rating for a product.
-While querying for a product it should also return the average rating along with product details.
-"""
-
-
 OPTIONAL = {"blank": True, "null": True}
 ACTIVE = ((0, 'Inactive'), (2, 'Active'))
 
